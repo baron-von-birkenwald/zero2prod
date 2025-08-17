57150c6dc5ff2876e4ad2b536d7bc4d34c4fa00a
Ethan Boss <ethan.boss@silverfern.com>
2025-08-16 21:31:50 -0400

    Auto-commit: Save changes before file operation

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -19,9 +19,14 @@ pub struct FormData {
     )
 )]
 pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> HttpResponse {
+    let name = match SubscriberName::parse(form.0.name) {
+        Ok(name) => name,
+        // Return early if the name is invalid, with a 400
+        Err(_) => return HttpResponse::BadRequest().finish(),
+    };
     let new_subscriber = NewSubscriber {
         email: form.0.email,
-        name: SubscriberName::parse(form.0.name),
+        name,
     };
     match insert_subscriber(&pool, &new_subscriber).await {
         Ok(_) => HttpResponse::Ok().finish(),
@@ -44,7 +49,7 @@ VALUES ($1, $2, $3, $4)
 "#,
         Uuid::new_v4(),
         new_subscriber.email,
-        new_subscriber.name.inner_ref(),
+        new_subscriber.name.as_ref(),
         Utc::now()
     )
     .execute(pool)
